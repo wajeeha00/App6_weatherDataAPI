3ad771295dd2f28ad5a56db5a86534818461c54f
wajeeha00 <wajeehaaftab7890@gmail.com>
2024-07-25 12:12:10 +0500

    ed

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, jsonify,render_template
+import pandas as pd
 
-app =Flask("website")
-@app.route("/home")
+app =Flask(__name__)
+@app.route("/")
 def home():
     return render_template("home.html")
 
-# @app.route("/about/")
-# def about():
-#     return render_template("about.html" )
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    print("station:",station)
+    print("date:",date)
+    filename = 'data_small\TG_STAID'+str(station).zfill(6)+'.txt'
+    df = pd.read_csv(filename, skiprows=20,  parse_dates=["    DATE"])
 
-# @app.route("/contact-us/")
-# def contact():
-#     return render_template("contact.html")
+    temperature = df.loc[df['    DATE']==date]['   TG'].squeeze() / 10
+    print(temperature)
+    return {"station":station,
+            "date":date,
+            "temperature":temperature}
 
-# @app.route("/store/")
-# def store():
-#     return render_template("store.html")
+if __name__ == "__main__":
 
-app.run(debug=True)
+    app.run(debug=True)
